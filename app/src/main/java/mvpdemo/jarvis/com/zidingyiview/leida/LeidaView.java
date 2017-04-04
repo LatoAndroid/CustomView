e791a047e1ce40a427005669cf44067a8204fe9f
jiangwei <jiangwei_android@163.com>
2017-04-04 23:27:32 +0800

    Initial commit

---
+++ app/src/main/java/mvpdemo/jarvis/com/zidingyiview/leida/LeidaView.java
@@ -0,0 +1,27 @@
+package mvpdemo.jarvis.com.zidingyiview.leida;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jiangwei on 2017/4/4.
+ */
+
+public class LeidaView extends View {
+    public LeidaView(Context context) {
+        this(context,null);
+    }
+
+    public LeidaView(Context context, AttributeSet attrs) {
+       this(context,attrs,0);
+    }
+
+    public LeidaView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //逻辑代码可以在这里写，最佳实践
+    }
+
+
+
+}
